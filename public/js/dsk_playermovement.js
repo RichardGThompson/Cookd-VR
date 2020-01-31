3d5dedc94197987fd1d7373d3549953f3df9b323
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2020-01-31 10:45:41 -0500

    can move the player with WASD.
    Going to see if click based movement would be better/smoother

--- public/js/dsk_playermovement.js
+++ public/js/dsk_playermovement.js
@@ -7,9 +7,38 @@ AFRAME.registerComponent('dsk_playermovement', {
             if(event.keyCode == 87)
             {
                 console.log('w pressed');
+                Context_AF.moveplayer(0, 1);
             }
-            
-
+        });
+        window.addEventListener('keydown', function(event) {
+            if(event.keyCode == 83)
+            {
+                console.log('s pressed');
+                Context_AF.moveplayer(0, -1);
+            }
+        });
+        window.addEventListener('keydown', function(event) {
+            if(event.keyCode == 65)
+            {
+                console.log('a pressed');
+                Context_AF.moveplayer(1, 0);
+            }
+        });
+        window.addEventListener('keydown', function(event) {
+            if(event.keyCode == 68)
+            {
+                console.log('d pressed');
+                Context_AF.moveplayer(-1, 0);
+            }
         });
     },
+
+    moveplayer: function(movex, movez)
+    {
+        let player = this.el;
+        let playerpos = player.getAttribute('position');
+        
+        player.setAttribute('animation', "property: position; to:"+(playerpos.x + movex) +", " + (playerpos.y) + ", " + (playerpos.z + movez) + "; loop:false; dur: 200; easing: linear");
+    },
+    
 });
