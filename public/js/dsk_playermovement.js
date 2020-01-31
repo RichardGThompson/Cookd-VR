8fce9c17c5e4b234151cfd356254c82d1db16ff3
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2020-01-31 11:48:34 -0500

    Update dsk_playermovement.js

--- public/js/dsk_playermovement.js
+++ public/js/dsk_playermovement.js
@@ -9,44 +9,8 @@ AFRAME.registerComponent('dsk_playermovement', {
             console.log(event.detail.intersection.point);
             Context_AF.moveplayerclick(event.detail.intersection.point);
         });
-
-        window.addEventListener('keydown', function(event) {
-            if(event.keyCode == 87)
-            {
-                console.log('w pressed');
-                Context_AF.moveplayer(0, 1);
-            }
-        });
-        window.addEventListener('keydown', function(event) {
-            if(event.keyCode == 83)
-            {
-                console.log('s pressed');
-                Context_AF.moveplayer(0, -1);
-            }
-        });
-        window.addEventListener('keydown', function(event) {
-            if(event.keyCode == 65)
-            {
-                console.log('a pressed');
-                Context_AF.moveplayer(1, 0);
-            }
-        });
-        window.addEventListener('keydown', function(event) {
-            if(event.keyCode == 68)
-            {
-                console.log('d pressed');
-                Context_AF.moveplayer(-1, 0);
-            }
-        });
     },
 
-    moveplayer: function(movex, movez)
-    {
-        let player = this.el;
-        let playerpos = player.getAttribute('position');
-        
-        player.setAttribute('animation', "property: position; to:"+(playerpos.x + movex) +", " + (playerpos.y) + ", " + (playerpos.z + movez) + "; loop:false; dur: 200; easing: linear");
-    },
     moveplayerclick: function(pointVector)
     {
         let player = document.querySelector('#player');
