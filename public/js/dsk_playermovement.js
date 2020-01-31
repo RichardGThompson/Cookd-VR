1145f5c528d3f3265166ce60f8abb11132725e74
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2020-01-31 11:41:10 -0500

    Player now moves to clicked point
    When clicking on the floor. Player animates to that new position

--- public/js/dsk_playermovement.js
+++ public/js/dsk_playermovement.js
@@ -1,7 +1,14 @@
 AFRAME.registerComponent('dsk_playermovement', {
+    dependencies:['raycaster'],
     schema: {},
     init : function() {
         const Context_AF = this;
+
+        Context_AF.el.addEventListener('click', function(event) {
+            
+            console.log(event.detail.intersection.point);
+            Context_AF.moveplayerclick(event.detail.intersection.point);
+        });
 
         window.addEventListener('keydown', function(event) {
             if(event.keyCode == 87)
@@ -39,6 +46,13 @@ AFRAME.registerComponent('dsk_playermovement', {
         let playerpos = player.getAttribute('position');
         
         player.setAttribute('animation', "property: position; to:"+(playerpos.x + movex) +", " + (playerpos.y) + ", " + (playerpos.z + movez) + "; loop:false; dur: 200; easing: linear");
+    },
+    moveplayerclick: function(pointVector)
+    {
+        let player = document.querySelector('#player');
+        playerpos = player.getAttribute('position');
+        
+        player.setAttribute('animation', "property: position; to:"+(pointVector.x) +", " + (playerpos.y) + ", " + (pointVector.z) + "; loop:false; dur: 200; easing: linear");
     },
     
 });
