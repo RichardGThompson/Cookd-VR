54311a7bec426aeff264aee513415347290ea192
Graham Thompson <graham.thompson.613@gmail.com>
2020-02-08 18:05:46 -0500

    VR v0.03
    - Improved object release performance.
    - Cleaned up code.

--- public/js/vr-grab.js
+++ public/js/vr-grab.js
@@ -1,31 +1,38 @@
+
+
 AFRAME.registerComponent('vr-grab', {
     init : function(){
         const Context_AF = this;
         var grabbing = false;
         var grabbed = false;
-        var grabbedID = "";
-        Context_AF.el.addEventListener('collide', function (e) {
-            //console.log(e.detail.body.el);    // Other entity, which playerEl touched.
-            const collidedEl = e.detail.body.el;
-            if(collidedEl.getAttribute('class') == 'grabbable' && grabbing == true && grabbed == false){
-                console.log('pickup');
-                grabbedID = collidedEl.getAttribute('id');
-                //<a-box constraint="target: #other-box;" dynamic-body /> 
-                console.log(this.getAttribute('id'));
-                Context_AF.el.setAttribute('constraint', 'target: #' + grabbedID);
+
+        //Check to see if the controller is touching a grabbable object.
+        Context_AF.el.addEventListener('collide', function(collidedObj){
+            //Get the element of the object the controller collided with.
+            const collidedEl = collidedObj.detail.body.el;
+            
+            //Check to see if the object is grabbable, if the hand is currently in a grabbing state, and if there is not an object currently in hand.
+            if(collidedEl.getAttribute('class') == 'grabbable' && grabbing && !grabbed){
+                //Attach a constraint to the hand.
+                Context_AF.el.setAttribute('constraint', 'target: #' + collidedEl.getAttribute('id'));
                 grabbed = true;
             }
-            if(grabbed == true && grabbing == false){
-                Context_AF.el.removeAttribute('constraint');
-                grabbed = false;
-            }
-        });
+        })
 
+        //Check to see if the hand is gripping down.
         Context_AF.el.addEventListener('gripdown', function(){
             grabbing = true;
         })
+
+        //Check to see if the hand is not gripping down.
         Context_AF.el.addEventListener('gripup', function(){
             grabbing = false;
+            //If there is an object grabbed, then drop it.
+            if(grabbed == true){
+                //Remove the object constraint from the controller.
+                Context_AF.el.removeAttribute('constraint');
+                grabbed = false;
+            }
         })
     },
 })
