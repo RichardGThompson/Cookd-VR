ceddeabff4e5800afa55f80637bbf67e889ce646
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2020-03-11 09:44:58 -0400

    NPCs now spawn at 8 different tables and cannot spawn if the table is full

--- public/js/dsk_npcspawning.js
+++ public/js/dsk_npcspawning.js
@@ -5,8 +5,10 @@ AFRAME.registerComponent('dsk_npcspawning', {
     schema: {
         counter: {type:'number', default:0},
         hasspawned: {type:'boolean', default: false},
-        table1taken: {type:'boolean', default:false},
-        table2taken: {type:'boolean', default:false},
+        tabletaken: {type:'array', default:[
+           
+        ]},
+        
     },
     init : function() {
         const Context_AF = this;
@@ -41,19 +43,41 @@ AFRAME.registerComponent('dsk_npcspawning', {
     SpawnNPC: function()
     {
         let Context_AF = this;
-        let NPC = document.createElement('a-entity');
-        let table1 = document.querySelector('#table1');
-        let table2 = document.querySelector('#table2');
+        
+        //let table1 = document.querySelector('#table1');
+        //let table2 = document.querySelector('#table2');
         let ground = document.querySelector('#scene');
-        table1pos = table1.getAttribute('position');
-        table2pos = table2.getAttribute('position');
-        randposition = Math.floor(Math.random() * 4);
-        console.log(randposition)
-        
-        
-        if(randposition <= 1 && Context_AF.data.table1taken == false)
+       // table1pos = table1.getAttribute('position');
+        //table2pos = table2.getAttribute('position');
+        randposition = Math.floor(Math.random() * 7);
+        let id = randposition + 1;
+        let table = document.querySelector('#table' + id);
+        if(Context_AF.data.tabletaken[id])
         {
+            console.log("butts")
+        }
+        else
+        {
+            Context_AF.data.tabletaken[id] = id;
+            tablepos = table.getAttribute('position');
+            console.log("Spawning at table 1");
+            let NPC = document.createElement('a-entity');
+            NPC.setAttribute('position', {x:tablepos.x + 1.9, y:1.5, z: tablepos.z});
+            NPC.setAttribute('dsk_ticketgenerating', {});
+            NPC.setAttribute('obj-model', {obj:'/assets/rocket.obj'});
+            NPC.setAttribute('scale', {x:0.01, y:0.01, z:0.01});
+            NPC.setAttribute('id', 'npc');
+            NPC.setAttribute('dynamic-body', 'shape: box; height: 1; width: 1; mass: 100');
+            NPC.setAttribute('constraint', 'target: #scene');
+            let scene = document.querySelector('a-scene');
+            scene.appendChild(NPC);
+        }
+        
+        /*if(randposition <= 1 && Context_AF.data.table1taken == false)
+        {
+            console.log("Spawning at table 1");
             Context_AF.data.table1taken = true;
+            let NPC = document.createElement('a-entity');
             NPC.setAttribute('position', {x:table1pos.x + 1.9, y:1.5, z: table1pos.z});
             NPC.setAttribute('dsk_ticketgenerating', {});
             NPC.setAttribute('obj-model', {obj:'/assets/rocket.obj'});
@@ -66,9 +90,11 @@ AFRAME.registerComponent('dsk_npcspawning', {
             scene.appendChild(NPC);
             //Context_AF.GenerateNPC(randposition);
         }
-        else if(randposition => 2 && Context_AF.data.table2taken == false)
+        if(randposition >= 2 && Context_AF.data.table2taken == false)
         {
+            console.log("Spawning at table 2");
             Context_AF.data.table2taken = true;
+            let NPC = document.createElement('a-entity');
             NPC.setAttribute('position', {x:table2pos.x - 1.9, y:1.5, z: table2pos.z});
             NPC.setAttribute('dsk_ticketgenerating', {});
             NPC.setAttribute('obj-model', {obj:'/assets/rocket.obj'});
@@ -84,21 +110,7 @@ AFRAME.registerComponent('dsk_npcspawning', {
         else
         {
             console.log("tables full");
-        }
+        }*/
         
     },
-    GenerateNPC: function(position){
-        
-
-        if(position <= 1)
-        {
-            
-        }
-        else
-        {
-            //console.log('time elapsed: ' + elapsed);
-            
-        }
-        
-    }
 });
