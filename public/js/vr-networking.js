6101c70e70b91c2384bf6c9fb11cf431514cff36
Graham Thompson <graham.thompson.613@gmail.com>
2020-03-13 19:11:18 -0400

    V0.12

--- public/js/vr-networking.js
+++ public/js/vr-networking.js
@@ -1,7 +1,17 @@
+let socket = io();
+AFRAME.registerComponent('vr-websocket',{
+    init:function(){
+        socket.on('connect', function(data){
+            console.log('connected');
+        })
+    }
+})
+
 AFRAME.registerComponent('vr-send-order', {
     init:function(){
         this.el.addEventListener('raycaster-intersection', function(hitObj){
             console.log('gotem');
+            console.log(hitObj);
         })
     }
 })
