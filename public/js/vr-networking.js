cc18faad487120d97816c2eab03da8d2ee9bcea7
Graham Thompson <graham.thompson.613@gmail.com>
2020-03-18 14:02:30 -0400

    V0.13

--- public/js/vr-networking.js
+++ public/js/vr-networking.js
@@ -8,10 +8,50 @@ AFRAME.registerComponent('vr-websocket',{
 })
 
 AFRAME.registerComponent('vr-send-order', {
+    schema:{
+        alreadySent:{type:'array'},
+    },
     init:function(){
+        console.log(this.data);
+        const dataTest = this.data.alreadySent;
+        var dataToAdd = null;
         this.el.addEventListener('raycaster-intersection', function(hitObj){
             console.log('gotem');
-            console.log(hitObj);
+            if(!(dataTest.includes(this.parentNode.getAttribute('id')))){
+                dataToAdd = this.parentNode.getAttribute('id');
+                //this.data.alreadySent.push(this.parentNode.getAttribute('id'));
+                var arrayString = '';
+                for(i = 0; i < this.parentNode.components['vr-burger-info'].data.burgerElements.length; i++){
+                    var ID = this.parentNode.components['vr-burger-info'].data.burgerElements[i].getAttribute('id');
+                    if(ID.includes('bb')){
+                        arrayString.concat('1,');
+                    }
+                    else if(ID.includes('ptty')){
+                        arrayString.concat('2,');
+                    }
+                    else if(ID.includes('chz')){
+                        arrayString.concat('3,');
+                    }
+                    else if(ID.includes('tb')){
+                        arrayString.concat('4,');
+                    }
+                }
+                console.log(arrayString);
+                console.log(this.parentNode.components['vr-burger-info'].data.burgerElements.toString());
+                socket.emit('doneOrder', this.parentNode.components['vr-burger-info'].data.burgerElements.toString());
+            }
+            // console.log('gotem');
+            // //console.log(hitObj);
+
+            // console.log(this.parentNode.components['vr-burger-info'].data.burgerElements);
+            // //
+            // console.log(this.parentNode);
+            
+            //document.querySelector('#rightHand').removeAttribute('constraint');
+
+            
+            // var element = document.getElementById(this.parentNode.getAttribute('id'));
+            // element.parentNode.removeChild(element);
         })
     }
 })
