892f3ee1f68beca5e06fad05aab6eb18bd2c9d2d
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2020-03-25 11:54:39 -0400

    UI now displays which table has which order and what the order is
    You also can only collect an order once.

--- public/js/dsk_getticket.js
+++ public/js/dsk_getticket.js
@@ -5,40 +5,28 @@ AFRAME.registerComponent('dsk_getticket', {
 
     init : function() {
         const Context_AF = this;
-
+        
         Context_AF.el.addEventListener('collide', function(event){
-            if(event.detail.body.el.getAttribute('visible') == false)
+            if(event.detail.body.el.getAttribute('visible') == false && 
+                event.detail.body.el.components['dsk_ticketgenerating'].data.hasticket == false)
             {
-                if(event.detail.body.el.components['dsk_ticketgenerating'].data.RonsCreamySurprise)
-                {
-                    let ticket = document.querySelector('#ticket');
-                    let ticketID = document.querySelector('#ticketName');
-                    ticket.setAttribute('value', ' ' + event.detail.body.el.components['RonsCreamySurprise'].data.ingredient + '');
-                    ticketID.setAttribute('value', 'Rons creamy surprise ');
-                    Context_AF.data.hasticket = true;
-                    //console.log(Context_AF.el.components);
-                    Context_AF.el.components['dsk_networkmanager'].data.orderstore = event.detail.body.el.components['RonsCreamySurprise'].data.name  + ',' + event.detail.body.el.components['RonsCreamySurprise'].data.ingredient;;
-                }
-                else if(event.detail.body.el.components['dsk_ticketgenerating'].data.Big_mac)
-                {
-                    let ticket = document.querySelector('#ticket');
-                    let ticketID = document.querySelector('#ticketName');
-                    ticket.setAttribute('value', ' ' + event.detail.body.el.components['bigmac'].data.ingredient + '');
-                    ticketID.setAttribute('value', 'Big Man ');
-                    Context_AF.data.hasticket = true;
-                    //console.log(Context_AF.el.components);
-                    Context_AF.el.components['dsk_networkmanager'].data.orderstore = event.detail.body.el.components['bigmac'].data.name + ',' + event.detail.body.el.components['bigmac'].data.ingredient;
-                }
-                else if(event.detail.body.el.components['dsk_ticketgenerating'].data.burger)
-                {
-                    let ticket = document.querySelector('#ticket');
-                    let ticketID = document.querySelector('#ticketName');
-                    ticket.setAttribute('value', ' ' + event.detail.body.el.components['burger'].data.ingredient + '');
-                    ticketID.setAttribute('value', 'Burger ');
-                    Context_AF.data.hasticket = true;
-                    //console.log(Context_AF.el.components);
-                    Context_AF.el.components['dsk_networkmanager'].data.orderstore = event.detail.body.el.components['burger'].data.name  + ',' + event.detail.body.el.components['burger'].data.ingredient;
-                }
+                
+
+                let ticket = document.querySelector('#ticket');
+                let ticketID = document.querySelector('#ticketName');
+                let OrdersTakenHolder = document.querySelector('#OrdersTakenHolder')
+                
+                let componentname = event.detail.body.el.components['dsk_ticketgenerating'].data.nameHolder;
+                let orderimage = document.querySelector('#' + componentname + event.detail.body.el.id)
+                orderimage.setAttribute('visible', 'true');
+                ticket.setAttribute('value', ' ' + event.detail.body.el.components[componentname].data.ingredient + '');
+                ticketID.setAttribute('value', event.detail.body.el.components[componentname].data.name);
+                
+                Context_AF.data.hasticket = true;
+                event.detail.body.el.components['dsk_ticketgenerating'].data.hasticket = true;
+                //console.log(Context_AF.el.components);
+                Context_AF.el.components['dsk_networkmanager'].data.orderstore = event.detail.body.el.components[componentname].data.name  + ',' + 
+                event.detail.body.el.components[componentname].data.ingredient;
             }
 
         });
--- public/js/dsk_ticketgenerating.js
+++ public/js/dsk_ticketgenerating.js
@@ -1,19 +1,18 @@
 AFRAME.registerComponent('dsk_ticketgenerating', {
     schema: {
         burger: {type:'boolean', default: false},
-        Big_mac: {type:'boolean', default: false},
-        RonsCreamySurprise: {type:'boolean', default: false},
+        CheeseBurger: {type:'boolean', default: false},
+        DoubleCheeseBurger: {type:'boolean', default: false},
+        LoadedCheeseBurger: {type:'boolean', default: false},
+        DoubleLoadedCheeseBurger: {type:'boolean', default: false},
+        NameHolder: {type:'string', default:''},
+        hasticket: {type:'boolean', default: false}
     },
 
     init : function() {
         const Context_AF = this;
-
-
-        //let NPC = document.querySelector('#npc');
         
-        
-        //console.log(Context_AF.el.data.positiony);
-        rand = Math.floor(Math.random() * 3) + 1;
+        rand = Math.floor(Math.random() * 5) + 1;
         if(rand == 1)
         {
             Context_AF.el.setAttribute('burger', {});
@@ -21,13 +20,23 @@ AFRAME.registerComponent('dsk_ticketgenerating', {
         }
         if(rand == 2)
         {
-            Context_AF.el.setAttribute('bigmac', {});
-            Context_AF.data.Big_mac = true;
+            Context_AF.el.setAttribute('CheeseBurger', {});
+            Context_AF.data.CheeseBurger = true;
         }
         if(rand == 3)
         {
-            Context_AF.el.setAttribute('RonsCreamySurprise', {});
-            Context_AF.data.RonsCreamySurprise = true;
+            Context_AF.el.setAttribute('DoubleCheeseBurger', {});
+            Context_AF.data.DoubleCheeseBurger = true;
+        }
+        if(rand == 4)
+        {
+            Context_AF.el.setAttribute('LoadedCheeseBurger', {});
+            Context_AF.data.LoadedCheeseBurger = true;
+        }
+        if(rand == 5)
+        {
+            Context_AF.el.setAttribute('DoubleLoadedCheeseBurger', {});
+            Context_AF.data.DoubleLoadedCheeseBurger = true;
         }
     
     
@@ -37,42 +46,75 @@ AFRAME.registerComponent('burger', {
     schema: {
         price: {type:'number', default:10},
         name: {type: 'string', default:'Burger'},
+        label: {type: 'string', default:'burger'},
         ingredient: {type:'string', default:'bun,patty,bun'}
     },
     init : function() {
         const Context_AF = this;
         
         console.log(Context_AF.data.ingredient);
-
+        Context_AF.el.components['dsk_ticketgenerating'].data.nameHolder = Context_AF.data.label;
         
     },
 });
-AFRAME.registerComponent('bigmac', {
+AFRAME.registerComponent('CheeseBurger', {
     schema: {
         price: {type:'number', default:5},
-        name: {type: 'string', default:'Big Mac'},
-        ingredient: {type:'string', default:'bun,patty,lettuce,sauce,bun'}
+        name: {type: 'string', default:'Cheese Burger'},
+        label: {type: 'string', default:'CheeseBurger'},
+        ingredient: {type:'string', default:'bun,patty,cheese,bun'}
     },
     init : function() {
         const Context_AF = this;
-        Context_AF.data.price = 2;
         console.log(Context_AF.data.ingredient);
+        Context_AF.el.components['dsk_ticketgenerating'].data.nameHolder = Context_AF.data.label;
+    },
+});
 
+AFRAME.registerComponent('DoubleCheeseBurger', {
+    schema: {
+        price: {type:'number', default:0},
+        name: {type: 'string', default:'Double Cheese Burger'},
+        label: {type: 'string', default:'DoubleCheeseBurger'},
+        ingredient: {type:'string', default:'bun,patty,patty,cheese,bun'}
+    },
+    init : function() {
+        const Context_AF = this;
+        
+        console.log(Context_AF.data.ingredient);
+        Context_AF.el.components['dsk_ticketgenerating'].data.nameHolder = Context_AF.data.label;
         
     },
 });
 
-AFRAME.registerComponent('RonsCreamySurprise', {
+AFRAME.registerComponent('LoadedCheeseBurger', {
     schema: {
         price: {type:'number', default:0},
-        name: {type: 'string', default:'Rons Creamy Surprise'},
-        ingredient: {type:'string', default:'Mayo'}
+        name: {type: 'string', default:'Loaded Cheese Burger'},
+        label: {type: 'string', default:'LoadedCheeseBurger'},
+        ingredient: {type:'string', default:'bun,patty,cheese,Lettuce,Onion,bun'}
     },
     init : function() {
         const Context_AF = this;
         
         console.log(Context_AF.data.ingredient);
+        Context_AF.el.components['dsk_ticketgenerating'].data.nameHolder = Context_AF.data.label;
+        
+    },
+});
 
+AFRAME.registerComponent('DoubleLoadedCheeseBurger', {
+    schema: {
+        price: {type:'number', default:0},
+        name: {type: 'string', default:'Double Loaded Cheese Burger'},
+        label: {type: 'string', default:'DoubleLoadedCheeseBurger'},
+        ingredient: {type:'string', default:'bun,patty,patty,cheese,Lettuce,Onion,bun'}
+    },
+    init : function() {
+        const Context_AF = this;
+        
+        console.log(Context_AF.data.ingredient);
+        Context_AF.el.components['dsk_ticketgenerating'].data.nameHolder = Context_AF.data.label;
         
     },
 });
